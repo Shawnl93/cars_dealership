e6abb23d872ca0e36a2cfc697e14f0d83c5d015a
Shawn Lee <Shawnl93@hotmail.com>
2022-10-12 15:34:57 -0600

    fixed my test set up

--- spec/features/cars/index_spec.rb
+++ spec/features/cars/index_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe 'the child index page' do
   describe 'As a visitor' do
     describe 'when I visit the child index page' do
       before :each do
-        # @dealership = Dealership.create!(name: "T3SLA", city: "Lone_tree", charging_stations: 5, leasing: true)
-        # @dealership_1 = Dealership.create!(name: "T3SL@", city: "Denver", charging_stations: 3, leasing: true)
-        @car_1 = Car.create!(make: "Tesla", model: "S", year: 2022, auto_pilot: true)
-        @car_2 = Car.create!(make: "Tesla", model: "3", year: 2021, auto_pilot: false)
-        @car_3 = Car.create!(make: "Tesla", model: "X", year: 2020, auto_pilot: true)
-        @car_4 = Car.create!(make: "Tesla", model: "Y", year: 2019, auto_pilot: false)
+        @dealership = Dealership.create!(name: "T3SLA", city: "Lone_tree", charging_stations: 5, leasing: true)
+        @dealership_1 = Dealership.create!(name: "T3SL@", city: "Denver", charging_stations: 3, leasing: true)
+        @car_1 = @dealership.cars.create!(make: "Tesla", model: "S", year: 2022, auto_pilot: true)
+        @car_2 = @dealership.cars.create!(make: "Tesla", model: "3", year: 2021, auto_pilot: false)
+        @car_3 = @dealership.cars.create!(make: "Tesla", model: "X", year: 2020, auto_pilot: true)
+        @car_4 = @dealership.cars.create!(make: "Tesla", model: "Y", year: 2019, auto_pilot: false)
       end
       it "see the child and their attributes" do
         visit "/cars"
