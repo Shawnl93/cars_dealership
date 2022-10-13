b02cc0443d3bc4f4d0464c833fccd5bd8d7b1330
Shawn Lee <Shawnl93@hotmail.com>
2022-10-12 22:07:17 -0600

    feat: dealership shows number of cars

--- spec/features/dealerships/show_spec.rb
+++ spec/features/dealerships/show_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe 'the parents show page' do
       before :each do
         @dealership = Dealership.create!(name: "T3SLA", city: "Lone_tree", charging_stations: 5, leasing: true)
         @dealership_1 = Dealership.create!(name: "T3SL@", city: "Denver", charging_stations: 3, leasing: true)
+        @car_1 = @dealership.cars.create!(make: "Tesla", model: "S", year: 2022, auto_pilot: true)
+        @car_2 = @dealership_1.cars.create!(make: "Tesla", model: "3", year: 2021, auto_pilot: false)
+        @car_3 = @dealership.cars.create!(make: "Tesla", model: "X", year: 2020, auto_pilot: true)
+        @car_4 = @dealership_1.cars.create!(make: "Tesla", model: "Y", year: 2019, auto_pilot: false)
         visit "/dealerships/#{@dealership.id}"
       end
       it "see the parent with that id and all their attributes" do
